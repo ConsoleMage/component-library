82a2ba5354df28bb8a22ed5930efd6e294ebea58
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-04-01 15:18:17 +0800

    banner done

--- banners/Banner.tsx
+++ banners/Banner.tsx
@@ -2,24 +2,73 @@ import React from "react"
 import classnames from "classnames";
 
 interface BannerProps {
-    className?: string;
-    variant?: string;
-    [key: string]: any;
-  }
+  className?: string;
+  variant?: string;
+  [key: string]: any;
+}
 
-const Banner: React.FC<BannerProps> = ({ children, className, variant, ...rest }) => {
-  const variantClass = variant && `${variant}`;
-  const allClasses = classnames(variantClass, className);
-  return (
-    <div className={allClasses} {...rest}>
-        <div>
+const Banner: React.FC<BannerProps> = ({ children, variant, ...rest }) => {
+  const banner = "banner";
+  const allClasses = classnames(variant, banner);
+
+  switch (variant) {
+    case "success":
+      return (
+        <div className={allClasses}>
+          <div>
             <img src="./img/Check circle.png" alt="" />
+          </div>
+          <div>
+            <p>
+              {rest.status}
+              {rest.text && <span>{rest.text}</span>}
+            </p>
+          </div>
         </div>
-        <div>
-            <p>Congratulations!<span>{children}</span></p>
+      )
+    case "warning":
+      return (
+        <div className={allClasses}>
+          <div>
+            <img src="./img/Exclamation.png" alt="" />
+          </div>
+          <div>
+            <p>
+              {rest.status}
+              {rest.text && <span>{rest.text}</span>}
+            </p>
+          </div>
         </div>
-    </div>
-)
+      )
+    case "error":
+      return (
+        <div className={allClasses}>
+          <div>
+            <img src="./img/X circle.png" alt="" />
+          </div>
+          <div>
+            <p>
+              {rest.status}
+              {rest.text && <span>{rest.text}</span>}
+            </p>
+          </div>
+        </div>
+      )
+    case "neutral":
+      return (
+        <div className={allClasses}>
+          <div>
+            <img src="./img/Icon.png" alt="" />
+          </div>
+          <div>
+            <p>
+              {rest.status}
+              {rest.text && <span>{rest.text}</span>}
+            </p>
+          </div>
+        </div>
+      )
+  }
 }
 
 export default Banner;
--- banners/index.tsx
+++ banners/index.tsx
@@ -3,9 +3,60 @@ import ReactDOM from 'react-dom/client';
 import Banner from "./Banner"
 
 function App() {
-  return (
-      <Banner variant="success">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.</Banner>
-  )
+
+  const allBanners = [
+    {
+      id: 1,
+      variant: "success",
+      status: "Congratulations!",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+    },
+    {
+      id: 2,
+      variant: "warning",
+      status: "Attention",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.",
+    },
+    {
+      id: 3,
+      variant: "error",
+      status: "There is a problem with your application",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam quo totam eius aperiam dolorum.",
+    },
+    {
+      id: 4,
+      variant: "neutral",
+      status: "Update available",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid pariatur, ipsum similique veniam.",
+    },
+    {
+      id: 5,
+      variant: "success",
+      status: "Congratulations!",
+    },
+    {
+      id: 6,
+      variant: "warning",
+      status: "Attention",
+    },
+    {
+      id: 7,
+      variant: "error",
+      status: "There is a problem with your application",
+    },
+    {
+      id: 8,
+      variant: "neutral",
+      status: "Update available",
+    },
+  ];
+    return (
+      <div>
+        {allBanners.map((item) => (
+          <Banner key={item.id} {...item} />
+        ))}
+      </div>
+    );
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
