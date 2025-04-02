8f5a0a35ea8f03a2013e9ddb11d91aff5db599eb
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-04-02 10:09:14 +0800

    s

--- cards/Card.tsx
+++ cards/Card.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { VscPassFilled } from "react-icons/vsc";
+
 
 export default function Card(props) {
   return (
     <div className="card">
-      <div className="icon"><VscPassFilled /></div>
+      <div className="icon">{props.icon}</div>
       <p className="title">{props.title}</p>
       <p className="description">{props.description}</p>
     </div>
--- cards/index.tsx
+++ cards/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Card from './Card';
+import { VscPassFilled } from "react-icons/vsc";
 
 function App() {
   return (
     <Card 
+      icon={<VscPassFilled />}
       title="Easy Deployment" 
       description="Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis."
     />
