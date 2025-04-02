3f4c84774549ca4dc0864a99746be3a7e94e32ed
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-04-02 20:10:33 +0800

    s

--- testimonials/Testimonial.tsx
+++ testimonials/Testimonial.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 
 export default function Testimonial(props) {
-  return (
-    <div className="testimonial">
-      <img className="logo" src={props.logo} alt="" />
-      <p className="text">{props.text}</p>
-      <div className="cite">
-        <span className="name">{props.name}</span>
-        <img className="divider" src={props.divider} alt=""/>
-        <span className="position">{props.position}</span>
+  if (props.img) {
+    return (
+      <div className="img-testimonial">
+        <img className="img" src={props.img} alt="" />
+        <div className="container" >
+          <img className="comma" src={props.comma} alt="" />
+          <p className="img-text">{props.text}</p>
+          <span className="img-name">{props.name}</span>
+          <span className="img-role">{props.role}</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="testimonial">
+        <img className="logo" src={props.logo} alt="" />
+        <p className="text">{props.text}</p>
+        <div className="cite">
+          <span className="name">{props.name}</span>
+          <img className="divider" src={props.divider} alt="" />
+          <span className="role">{props.role}</span>
+        </div>
+      </div>
+    );
+  }
 };
--- testimonials/index.tsx
+++ testimonials/index.tsx
@@ -4,16 +4,27 @@ import Testimonial from './Testimonial';
 
 function App() {
   return (
-    <Testimonial
-      logo="brand\Logo.png"
-      text="
-        “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa 
-        sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”
-      "
-      name="May Andersons"
-      divider="brand\Divider.png"
-      position="Workcation, CTO"
-    />
+    <>
+      <Testimonial
+        img="img\Image.png"
+        comma="brand\Icon.png"
+        logo="brand\Logo.png"
+        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna nulla vitae laoreet augue. 
+        Amet feugiat est integer dolor auctor adipiscing nunc urna, sit."
+        name="May Andersons"
+        role="Workcation, CTO"
+      />
+      <Testimonial
+        logo="brand\Logo.png"
+        text="
+      “Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa 
+      sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis.”
+    "
+        name="May Andersons"
+        divider="brand\Divider.png"
+        role="Workcation, CTO"
+      />
+    </>
   )
 }
 
