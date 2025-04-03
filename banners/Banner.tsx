a330a31c423f627059427a6440a3518200da27cc
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-04-03 11:07:28 +0800

    s

--- banners/Banner.tsx
+++ banners/Banner.tsx
@@ -16,7 +16,7 @@ const Banner: React.FC<BannerProps> = ({ children, variant, ...rest }) => {
       return (
         <div className={allClasses}>
           <div>
-            <img src="/img/Check circle.png" alt="" />
+            <img src="./img/Check circle.png" alt="" />
           </div>
           <div>
             <p>
@@ -30,7 +30,7 @@ const Banner: React.FC<BannerProps> = ({ children, variant, ...rest }) => {
       return (
         <div className={allClasses}>
           <div>
-            <img src="/img/Exclamation.png" alt="" />
+            <img src="./img/Exclamation.png" alt="" />
           </div>
           <div>
             <p>
