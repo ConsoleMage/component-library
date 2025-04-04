52068a77346fd078f7d42bd12a4f73a34c144ae2
ConsoleMage <128290297+ConsoleMage@users.noreply.github.com>
2025-04-04 08:19:24 +0800

    s

--- cards/Card.tsx
+++ cards/Card.tsx
@@ -3,10 +3,12 @@ import React from "react";
 
 export default function Card(props) {
   return (
-    <div className="card">
-      <div className="icon">{props.icon}</div>
-      <p className="title">{props.title}</p>
-      <p className="description">{props.description}</p>
+    <div className="container">
+      <div className="card">
+        <div className="icon">{props.icon}</div>
+        <p className="title">{props.title}</p>
+        <p className="description">{props.description}</p>
+      </div>
     </div>
   );
 };
